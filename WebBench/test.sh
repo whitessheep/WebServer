0454602ccc87267d5f937a8404290cebbb09f472
WhiteSheep <260886429@qq.com>
2021-08-05 23:47:45 +0800

    WebBench

---
+++ WebBench/test.sh
@@ -0,0 +1,3 @@
+./webbench -t 60 -c 100 -2 --get  http://localhost:12345/hello;
+ ./webbench -t 60 -c 1000 -2 --get  http://localhost:12345/hello;
+ ./webbench -t 60 -c 10000 -2 --get  http://localhost:12345/hello;
